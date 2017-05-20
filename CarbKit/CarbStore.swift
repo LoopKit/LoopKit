ea810d6cb7370549e161d8f42efdd0ad95e86281
Michael Pangburn <michaelpangburn@comcast.net>
2017-05-19 18:35:02 -0700

    Made CarbStore's defaultAbsorptionTimes mutable

--- CarbKit/CarbStore.swift
+++ CarbKit/CarbStore.swift
@@ -110,7 +110,13 @@ public final class CarbStore: HealthKitSampleStore {
     }
 
     /// A trio of default carbohydrate absorption times. Defaults to 2, 3, and 4 hours.
-    public let defaultAbsorptionTimes: DefaultAbsorptionTimes
+    public var defaultAbsorptionTimes: DefaultAbsorptionTimes {
+        didSet {
+            dataAccessQueue.async {
+                self.clearCalculationCache()
+            }
+        }
+    }
 
     /// Insulin-to-glucose sensitivity
     public var insulinSensitivitySchedule: InsulinSensitivitySchedule? {
