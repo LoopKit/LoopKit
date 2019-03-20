b7ddb04a18fabcc2eabb5ecf5e0a3ea3838cabc3
Pete Schwamb <pete@schwamb.net>
2019-03-20 08:12:34 -0500

    Mark delegate method from public protocol public

--- LoopKitUI/View Controllers/DateAndDurationTableViewController.swift
+++ LoopKitUI/View Controllers/DateAndDurationTableViewController.swift
@@ -88,7 +88,7 @@ public class DateAndDurationTableViewController: UITableViewController {
 }
 
 extension DateAndDurationTableViewController: DatePickerTableViewCellDelegate {
-    func datePickerTableViewCellDidUpdateDate(_ cell: DatePickerTableViewCell) {
+    public func datePickerTableViewCellDidUpdateDate(_ cell: DatePickerTableViewCell) {
         switch inputMode {
         case .date(_, mode: let mode):
             inputMode = .date(cell.date, mode: mode)
