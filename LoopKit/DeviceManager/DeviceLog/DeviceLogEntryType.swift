4657468d5c3568d59cd3d5acca1fe0d3d01ff108
Rick Pasetto <rickpasetto@yahoo.com>
2020-02-13 13:03:59 -0800

    LOOP-981: Adds 'connection' DeviceLogEntryType (#51)
    * LOOP-981: Adds 'connection' DeviceLogEntryType
    
    * Add comments describing the types.
    
    * PR Feedback: added 'alert' type as well
    
    * Per discussion with Nate, removing 'alert' because it isn't strictly "device comms"

--- LoopKit/DeviceManager/DeviceLog/DeviceLogEntryType.swift
+++ LoopKit/DeviceManager/DeviceLog/DeviceLogEntryType.swift
@@ -9,9 +9,16 @@
 import Foundation
 
 public enum DeviceLogEntryType: String {
+    /// Log entry related to data or commands sent to the device.
     case send
+    /// Log entry related to data or events received from the device.
     case receive
+    /// Log entry related to any errors from the device's SDK or the device itself.
     case error
+    /// Log entry related to a delegate call from the device's SDK (for example, acknowledgement of receiving a command).
     case delegate
+    /// Log entry related to a response from a delegate call (for example, acknowledgement of executing a command).
     case delegateResponse
+    /// Log entry related to any device connection activities (e.g. scanning, connecting, disconnecting, reconnecting, etc.).
+    case connection
 }
