71db4e525107f8cb2f25949f453c49136dcb8ac8
mpangburn <mjpangbu@calpoly.edu>
2020-02-26 18:05:14 -0800

    Add optional thumb to SegmentedGaugeBarView

--- LoopKitUI/Views/SegmentedGaugeBarView.swift
+++ LoopKitUI/Views/SegmentedGaugeBarView.swift
@@ -69,11 +69,27 @@ public class SegmentedGaugeBarView: UIView {
     @IBInspectable
     public var progress: Double {
         get {
-            return Double(gaugeLayer.progress)
+            if displaysThumb {
+                return Double(fractionThrough(thumb.center.x, in: thumbCenterXRange))
+            } else {
+                return visualProgress
+            }
         }
         set {
-            gaugeLayer.progress = CGFloat(newValue)
+            if displaysThumb {
+                thumb.center.x = interpolatedValue(at: CGFloat(newValue), through: thumbCenterXRange).clamped(to: thumbCenterXRange)
+                // Push the gauge progress behind the thumb, ensuring the cap rounding is not visible.
+                let gaugeX = thumb.center.x + 0.25 * thumb.frame.width
+                visualProgress = newValue == 0 ? 0 : Double(fractionThrough(gaugeX, in: gaugeXRange))
+            } else {
+                visualProgress = newValue
+            }
         }
+    }
+
+    private var visualProgress: Double {
+        get { Double(gaugeLayer.progress) }
+        set { gaugeLayer.progress = CGFloat(newValue) }
     }
 
     public weak var delegate: SegmentedGaugeBarViewDelegate?
@@ -96,26 +112,109 @@ public class SegmentedGaugeBarView: UIView {
         setupPanGestureRecognizer()
     }
 
+    private lazy var thumb = ThumbView()
+
+    public var displaysThumb = false {
+        didSet {
+            if displaysThumb {
+                assert(numberOfSegments == 1, "Thumb only supported for single-segment gauges")
+                if thumb.superview == nil {
+                    addSubview(thumb)
+                }
+            } else {
+                thumb.removeFromSuperview()
+            }
+        }
+    }
+
+    private var panGestureRecognizer: UIPanGestureRecognizer?
+
     private func setupPanGestureRecognizer() {
         let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        panGestureRecognizer = pan
         addGestureRecognizer(pan)
     }
 
     @objc private func handlePan(_ recognizer: UIPanGestureRecognizer) {
         switch recognizer.state {
         case .began, .changed:
+            guard recognizer.numberOfTouches == 1 else {
+                break
+            }
+
             let location = recognizer.location(ofTouch: 0, in: self)
-            let fractionThrough = Double(location.x / frame.width)
+            let gaugeFillFraction = fractionThrough(location.x, in: gaugeXRange)
             let oldValue = progress
-            progress = (fractionThrough * Double(numberOfSegments)).clamped(to: 0...Double(numberOfSegments))
-            delegate?.segmentedGaugeBarView(self, didUpdateProgressFrom: oldValue, to: progress)
+            let newValue = (Double(gaugeFillFraction) * Double(numberOfSegments)).clamped(to: 0...Double(numberOfSegments))
+            CATransaction.withoutActions {
+                progress = newValue
+            }
+
+            delegate?.segmentedGaugeBarView(self, didUpdateProgressFrom: oldValue, to: newValue)
+        case .ended:
+            uglyWorkaroundToForceRedraw()
         default:
             break
+        }
+    }
+
+    private func uglyWorkaroundToForceRedraw() {
+        // Resolves an issue--most of the time--where dragging _very_ rapidly then releasing
+        // can cause the gauge layer to fall behind a cycle in rendering.
+        // - `setNeedsDisplay()` is insufficient.
+        // - Adding additional executions (delay times) catches the issue with a greater probability,
+        //   with diminishing returns after four executions (by observation).
+        for (index, delayMS) in [10, 25, 50, 100].enumerated() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(delayMS)) {
+                CATransaction.withoutActions {
+                    self.progress = index % 2 == 0 ? self.progress.nextUp : self.progress.nextDown
+                }
+            }
         }
     }
 
     override public func layoutSubviews() {
         super.layoutSubviews()
         layer.cornerRadius = frame.height / 2
+
+        updateThumbPosition()
+    }
+
+    private var gaugeXRange: ClosedRange<CGFloat> {
+        (bounds.minX + 2 * borderWidth)...(bounds.maxX - 2 * borderWidth)
+    }
+
+    private var thumbCenterXRange: ClosedRange<CGFloat> {
+        let radius = thumb.bounds.width / 2
+        return (bounds.minX + 2 * borderWidth + radius)...(bounds.maxX - 2 * borderWidth - radius)
+    }
+
+    private func updateThumbPosition() {
+        guard displaysThumb else {
+            return
+        }
+
+        let diameter = bounds.height - 2 * borderWidth
+        thumb.bounds.size = CGSize(width: diameter, height: diameter)
+        let xPosition = CGFloat(progress) * (thumbCenterXRange.upperBound - thumbCenterXRange.lowerBound) + thumbCenterXRange.lowerBound
+        thumb.center = CGPoint(x: xPosition, y: bounds.midY)
+    }
+
+    public func cancelActiveTouches() {
+        guard panGestureRecognizer?.isEnabled == true else {
+            return
+        }
+
+        panGestureRecognizer?.isEnabled = false
+        panGestureRecognizer?.isEnabled = true
+    }
+}
+
+fileprivate extension CATransaction {
+    static func withoutActions(_ execute: () -> Void) {
+        begin()
+        setValue(true, forKey: kCATransactionDisableActions)
+        execute()
+        commit()
     }
 }
