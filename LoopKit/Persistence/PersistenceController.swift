642066d9c2064826257e343ad6e240aa548b3918
Anna <anna@tidepool.org>
2020-09-20 15:16:31 -0700

    Fix core data links

--- LoopKit/Persistence/PersistenceController.swift
+++ LoopKit/Persistence/PersistenceController.swift
@@ -181,7 +181,7 @@ public final class PersistenceController {
                 // Ignore errors here, let Core Data explain the problem
             }
 
-            let storeURL = directoryURL.appendingPathComponent("Modelv3.sqlite")
+            let storeURL = directoryURL.appendingPathComponent("Modelv4.sqlite")
 
             do {
                 try coordinator.addPersistentStore(ofType: NSSQLiteStoreType,
