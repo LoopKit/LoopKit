5067f2df00684777b83028b3d8df19a330ea70de
Nathaniel Hamming <nate@hmtconsulting.org>
2020-11-12 15:45:20 -0400

    [LOOP-2242] Display of no value

--- LoopKitUI/Views/SettingsTableViewCell.swift
+++ LoopKitUI/Views/SettingsTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public class SettingsTableViewCell: UITableViewCell {
     public static let EnabledString = LocalizedString("Enabled", comment: "The detail text describing an enabled setting")
-    public static let NoValueString = LocalizedString("—", comment: "The detail text representing no value")
+    public static let NoValueString = LocalizedString("–", comment: "The detail text representing no value")
     public static let TapToSetString = LocalizedString("Tap to set", comment: "The empty-state text for a configuration value")
 
     public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
