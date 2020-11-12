41f5a8bd704fc3bac62987a387579ada0533b5c5
Nathaniel Hamming <nate@hmtconsulting.org>
2020-11-12 09:58:55 -0400

    renaming suspend threshold -> glucose safety limit (#273)

--- LoopKit/TherapySetting.swift
+++ LoopKit/TherapySetting.swift
@@ -32,7 +32,7 @@ public extension TherapySetting {
         case .workoutCorrectionRangeOverride:
             return String(format: LocalizedString("%@ Range", comment: "Format for correction range override therapy setting card"), CorrectionRangeOverrides.Preset.workout.title)
         case .suspendThreshold:
-            return LocalizedString("Suspend Threshold", comment: "Title text for suspend threshold")
+            return LocalizedString("Glucose Safety Limit", comment: "Title text for glucose safety limit")
         case .basalRate:
             return LocalizedString("Basal Rates", comment: "Title text for basal rates")
         case .deliveryLimits:
@@ -64,7 +64,7 @@ public extension TherapySetting {
         case .workoutCorrectionRangeOverride:
             return LocalizedString("Temporarily raise your glucose target before, during, or after physical activity to reduce the risk of low glucose events.", comment: "Descriptive text for workout correction range override")
         case .suspendThreshold:
-            return LocalizedString("When your glucose is predicted to go below this value, the app will recommend a basal rate of 0 U/hr and will not recommend a bolus.", comment: "Descriptive text for suspend threshold")
+            return LocalizedString("Tidepool Loop will deliver basal and recommend bolus insulin only if your glucose is predicted to be above this limit for the next three hours.", comment: "Descriptive text for glucose safety limit")
         case .basalRate:
             return LocalizedString("Your basal rate of insulin is the number of units per hour that you want to use to cover your background insulin needs.", comment: "Descriptive text for basal rate")
         case .deliveryLimits:
--- LoopKitUI/Views/Settings Editors/SuspendThresholdEditor.swift
+++ LoopKitUI/Views/Settings Editors/SuspendThresholdEditor.swift
@@ -197,7 +197,7 @@ public struct SuspendThresholdEditor: View {
 
     private func confirmationAlert() -> SwiftUI.Alert {
         SwiftUI.Alert(
-            title: Text("Save Suspend Threshold?", comment: "Alert title for confirming a suspend threshold outside the recommended range"),
+            title: Text("Save Glucose Safety Limit?", comment: "Alert title for confirming a glucose safety limit outside the recommended range"),
             message: Text(TherapySetting.suspendThreshold.guardrailSaveWarningCaption),
             primaryButton: .cancel(Text("Go Back")),
             secondaryButton: .default(
@@ -235,9 +235,9 @@ struct SuspendThresholdGuardrailWarning: View {
     private var title: Text {
         switch safetyClassificationThreshold {
         case .minimum, .belowRecommended:
-            return Text("Low Suspend Threshold", comment: "Title text for the low suspend threshold warning")
+            return Text("Low Glucose Safety Limit", comment: "Title text for the low glucose safety limit warning")
         case .aboveRecommended, .maximum:
-            return Text("High Suspend Threshold", comment: "Title text for the high suspend threshold warning")
+            return Text("High Glucose Safety Limit", comment: "Title text for the high glucose safety limit warning")
         }
     }
 }
