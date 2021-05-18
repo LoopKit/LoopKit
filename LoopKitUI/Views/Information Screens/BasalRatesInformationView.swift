2ceacf61e84ef3df8e4871e168da2282d8adbd9e
Nathaniel Hamming <nate@hmtconsulting.org>
2021-05-18 09:09:51 -0300

    [LOOP-2531] use appName instead of Loop (#356)

--- LoopKitUI/Views/Information Screens/BasalRatesInformationView.swift
+++ LoopKitUI/Views/Information Screens/BasalRatesInformationView.swift
@@ -14,6 +14,7 @@ public struct BasalRatesInformationView: View {
     var mode: SettingsPresentationMode
     
     @Environment(\.presentationMode) var presentationMode
+    @Environment(\.appName) var appName
     
     public init(onExit: (() -> Void)?, mode: SettingsPresentationMode = .acceptanceFlow) {
         self.onExit = onExit
@@ -32,9 +33,29 @@ public struct BasalRatesInformationView: View {
     private var text: some View {
         VStack(alignment: .leading, spacing: 25) {
             Text(LocalizedString("Your Basal Rate of insulin is the number of units per hour that you want to use to cover your background insulin needs.", comment: "Information about basal rates"))
-            Text(LocalizedString("Loop supports 1 to 48 rates per day.", comment: "Information about max number of basal rates"))
+            Text(String(format: LocalizedString("%1$@ supports 1 to 48 rates per day.", comment: "Information about max number of basal rates"), appName))
             Text(LocalizedString("The schedule starts at midnight and cannot contain a rate of 0 U/hr.", comment: "Information about basal rate scheduling"))
         }
         .foregroundColor(.secondary)
     }
 }
+
+struct BasalRatesInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            BasalRatesInformationView(onExit: nil)
+        }
+        .colorScheme(.light)
+        .previewDevice(PreviewDevice(rawValue: "iPod touch (7th generation)"))
+        .previewDisplayName("SE light")
+        
+        NavigationView {
+            BasalRatesInformationView(onExit: nil)
+        }
+        .preferredColorScheme(.dark)
+        .colorScheme(.dark)
+        .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+        .previewDisplayName("12 Pro dark")
+    }
+}
+
