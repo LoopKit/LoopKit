9d7162764b6ef59a8cb5c65df86907cddd6f7788
Rick Pasetto <rick@tidepool.org>
2021-08-12 10:31:41 -0700

    Adds very rudimentary migration test (#385)
    * Adds very rudimentary migration test
    
    * minor tweak to naming
    
    * PR Feedback

---
+++ LoopKitHostedTests/CoreDataMigrationTests.swift
@@ -0,0 +1,64 @@
+//
+//  CoreDataMigrationTests.swift
+//  LoopKitHostedTests
+//
+//  Created by Rick Pasetto on 8/9/21.
+//  Copyright © 2021 LoopKit Authors. All rights reserved.
+//
+
+import CoreData
+import HealthKit
+import XCTest
+@testable import LoopKit
+
+class CoreDataV1MigrationTests: XCTestCase {
+    
+    var cacheStore: PersistenceController!
+    let fileManager = FileManager()
+    static let bundleURL = Bundle(for: CoreDataV1MigrationTests.self).bundleURL
+
+    override func setUpWithError() throws {
+        try? fileManager.removeItem(at: Self.bundleURL.appendingPathComponent("Model.sqlite"))
+        try fileManager.copyItem(at: Self.bundleURL.appendingPathComponent("Model.sqlite.v1.original"),
+                                 to: Self.bundleURL.appendingPathComponent("Model.sqlite"))
+    }
+
+    override func tearDownWithError() throws {
+        
+        // remove all stores
+        try cacheStore.managedObjectContext.persistentStoreCoordinator?.persistentStores.forEach { store in
+            try cacheStore.managedObjectContext.persistentStoreCoordinator?.remove(store)
+        }
+        
+        try fileManager.removeItem(at: Self.bundleURL.appendingPathComponent("Model.sqlite"))
+        cacheStore = nil
+    }
+
+    func testV1Migration() throws {
+        let e = expectation(description: "\(#function): init")
+        cacheStore = PersistenceController.init(directoryURL: Self.bundleURL)
+        var error: Error?
+        cacheStore.onReady {
+            if let err = $0 {
+                error = err
+                XCTFail("Error opening \(Self.bundleURL): \(err)")
+            }
+            e.fulfill()
+        }
+        wait(for: [e], timeout: 3.0)
+        if let error = error { throw error }        
+        var entries: [CachedCarbObject]!
+        let e0 = expectation(description: "\(#function): fetch")
+        cacheStore.managedObjectContext.performAndWait {
+            do {
+                entries = try cacheStore.managedObjectContext.fetch(CachedCarbObject.fetchRequest())
+            } catch let err {
+                error = err
+            }
+            e0.fulfill()
+        }
+        wait(for: [e0], timeout: 1.0)
+        if let error = error { throw error }
+        XCTAssertEqual(1, entries.count)
+    }
+}
