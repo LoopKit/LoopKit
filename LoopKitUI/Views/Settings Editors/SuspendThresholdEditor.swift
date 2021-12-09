5242f11702dc8f900b4febc72502f97c0350ffd2
Rick Pasetto <rick@tidepool.org>
2021-12-09 10:45:44 -0800

    LOOP-3870: Switch QuantityPicker to use new UIKit-based "SizeablePicker"  (#425)
    * LOOP-3870: Switch QuantityPicker to use new UIKit-based "SizeablePicker"
    
    @ps2 figured out how to force a UIKit-based UIPickerView to obey it's touchable area based on it's size (Thanks, Pete!)
    
    * comments
    
    * Fix missing label
    
    * One fix for Suspend Threshold (Glucose Safety Limit), but still missing label there
    
    * Kind of an ugly hack but fixes the missing unit label for this specific case
    
    * PR Feedback

--- LoopKitUI/Views/Settings Editors/SuspendThresholdEditor.swift
+++ LoopKitUI/Views/Settings Editors/SuspendThresholdEditor.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import HealthKit
 import LoopKit
 
-
+// Also known as "Glucose Safety Limit"
 public struct SuspendThresholdEditor: View {
     @EnvironmentObject private var displayGlucoseUnitObservable: DisplayGlucoseUnitObservable
 
@@ -109,9 +109,7 @@ public struct SuspendThresholdEditor: View {
                                 bounds: viewModel.guardrail.absoluteBounds.lowerBound...viewModel.maxSuspendThresholdValue
                             )
                             // Prevent the picker from expanding the card's width on small devices
-                            .frame(maxWidth: UIScreen.main.bounds.width - 48)
-                            .clipped()
-                            .compositingGroup()
+                            .frame(maxWidth: 220)
                         }
                     )
                 }
