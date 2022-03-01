a10e951ac45b0165b2f6083a6c1bfa90dedf4677
Pete Schwamb <pete@schwamb.net>
2022-03-01 07:45:19 -0600

    Cleanup commented code

--- LoopKit/CarbKit/CachedCarbObject+CoreDataClass.swift
+++ LoopKit/CarbKit/CachedCarbObject+CoreDataClass.swift
@@ -91,8 +91,6 @@ extension CachedCarbObject {
 
     // HealthKit
     func create(from sample: HKQuantitySample, on date: Date = Date()) {
-        //precondition(!sample.createdByCurrentApp)
-
         self.absorptionTime = sample.absorptionTime
         self.createdByCurrentApp = sample.createdByCurrentApp
         self.foodType = sample.foodType
