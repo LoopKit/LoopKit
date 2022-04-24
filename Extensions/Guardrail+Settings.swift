4a4161b9c75d8985db91deaea012b0a71002b684
marionbarker <marionbarker@earthlink.net>
2022-04-24 14:07:27 -0700

    Propose wider limits to support experienced as well as new loopers

--- Extensions/Guardrail+Settings.swift
+++ Extensions/Guardrail+Settings.swift
@@ -9,7 +9,7 @@
 import HealthKit
 
 public extension Guardrail where Value == HKQuantity {
-    static let suspendThreshold = Guardrail(absoluteBounds: 67...110, recommendedBounds: 74...80, unit: .milligramsPerDeciliter, startingSuggestion: 80)
+    static let suspendThreshold = Guardrail(absoluteBounds: 65...120, recommendedBounds: 75...110, unit: .milligramsPerDeciliter, startingSuggestion: 90)
 
     static func maxSuspendThresholdValue(correctionRangeSchedule: GlucoseRangeSchedule?, preMealTargetRange: ClosedRange<HKQuantity>?, workoutTargetRange: ClosedRange<HKQuantity>?) -> HKQuantity {
 
@@ -23,7 +23,7 @@ public extension Guardrail where Value == HKQuantity {
         .min()!
     }
 
-    static let correctionRange = Guardrail(absoluteBounds: 87...180, recommendedBounds: 100...115, unit: .milligramsPerDeciliter, startingSuggestion: 100)
+    static let correctionRange = Guardrail(absoluteBounds: 75...180, recommendedBounds: 80...150, unit: .milligramsPerDeciliter, startingSuggestion: 110)
 
     static func minCorrectionRangeValue(suspendThreshold: GlucoseThreshold?) -> HKQuantity {
         return [
