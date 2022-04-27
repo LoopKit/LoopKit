046226b0bef259b113040d83b9004bafcce0712b
marionbarker <marionbarker@earthlink.net>
2022-04-27 16:59:43 -0700

    update: correction range information screen

--- LoopKitUI/Views/Information Screens/CorrectionRangeInformationView.swift
+++ LoopKitUI/Views/Information Screens/CorrectionRangeInformationView.swift
@@ -35,9 +35,12 @@ public struct CorrectionRangeInformationView: View {
         VStack(alignment: .leading, spacing: 25) {
             Text(LocalizedString("If you've used a CGM before, you're likely familiar with target range as a wide range of values you'd like for your glucose notification alerts, such as 70-180 mg/dL or 90-200 mg/dL.", comment: "Information about target range"))
             Text(LocalizedString("A Correction Range is different. This will be a narrower range.", comment: "Information about differences between target range and correction range"))
-            .bold()
-            Text(String(format: LocalizedString("For this range, choose the specific glucose value (or range of values) that you want %1$@ to aim for in adjusting your basal insulin.", comment: "Information about correction range format (1: app name)"), appName))
+                .bold()
+            Text(String(format: LocalizedString("For this range, choose the specific glucose value (or range of values) that you want %1$@ to aim for in adjusting your insulin delivery.", comment: "Information about correction range format (1: app name)"), appName))
             Text(LocalizedString("Your healthcare provider can help you choose a Correction Range that's right for you.", comment: "Disclaimer"))
+                .bold()
+            Text(TherapySetting.addNewEntry.descriptiveText(appName: appName))
+            Text(TherapySetting.editDeleteEntry.descriptiveText(appName: appName))
         }
         .foregroundColor(.secondary)
     }
