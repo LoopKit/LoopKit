e2c82147782f05553d02ceff893ba4a4f56da892
Nathaniel Hamming <nate@hmtconsulting.org>
2022-07-19 14:25:40 -0300

    [COASTAL-969] System time offset detected (#463)
    * pump manager delegate reports when system time offset occurs
    
    * provide timeInterval instead of indicator
    
    * added comment to clarify detected system time offset value

--- LoopKit/DeviceManager/PumpManager.swift
+++ LoopKit/DeviceManager/PumpManager.swift
@@ -64,6 +64,9 @@ public protocol PumpManagerDelegate: DeviceManagerDelegate, PumpManagerStatusObs
     func pumpManagerDidUpdateState(_ pumpManager: PumpManager)
 
     func startDateToFilterNewPumpEvents(for manager: PumpManager) -> Date
+
+    /// Indicates the system time offset from a trusted time source. If the return value is added to the system time, the result is the trusted time source value. If the trusted time source is earlier than the system time, the return value is negative.
+    var detectedSystemTimeOffset: TimeInterval { get }
 }
 
 
