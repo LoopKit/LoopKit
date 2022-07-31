3cef4d343cdd0701b276c038d414766375b3fe90
Pete Schwamb <pete@schwamb.net>
2022-07-31 09:16:19 -0500

    Handle previous notification settings without time sensitive and scheduled delivery members

--- LoopKit/NotificationSettings.swift
+++ LoopKit/NotificationSettings.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UserNotifications
 
-public struct NotificationSettings: Codable, Equatable {
+public struct NotificationSettings: Equatable {
     public enum AuthorizationStatus: String, Codable {
         case notDetermined
         case denied
@@ -143,3 +143,73 @@ public struct NotificationSettings: Codable, Equatable {
     }
 }
 
+
+extension NotificationSettings: Codable {
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.init(
+            authorizationStatus: try container.decode(AuthorizationStatus.self, forKey: .authorizationStatus),
+            soundSetting: try container.decode(NotificationSetting.self, forKey: .soundSetting),
+            badgeSetting: try container.decode(NotificationSetting.self, forKey: .badgeSetting),
+            alertSetting: try container.decode(NotificationSetting.self, forKey: .alertSetting),
+            notificationCenterSetting: try container.decode(NotificationSetting.self, forKey: .notificationCenterSetting),
+            lockScreenSetting: try container.decode(NotificationSetting.self, forKey: .lockScreenSetting),
+            carPlaySetting: try container.decode(NotificationSetting.self, forKey: .carPlaySetting),
+            alertStyle: try container.decode(AlertStyle.self, forKey: .alertStyle),
+            showPreviewsSetting: try container.decode(ShowPreviewsSetting.self, forKey: .showPreviewsSetting),
+            criticalAlertSetting: try container.decode(NotificationSetting.self, forKey: .criticalAlertSetting),
+            providesAppNotificationSettings: try container.decode(Bool.self, forKey: .providesAppNotificationSettings),
+            announcementSetting: try container.decode(NotificationSetting.self, forKey: .announcementSetting),
+            timeSensitiveSetting: try container.decodeIfPresent(NotificationSetting.self, forKey: .timeSensitiveSetting) ?? .unknown,
+            scheduledDeliverySetting: try container.decodeIfPresent(NotificationSetting.self, forKey: .scheduledDeliverySetting) ?? .unknown)
+    }
+
+//    public func encode(to encoder: Encoder) throws {
+//        let bloodGlucoseUnit = self.bloodGlucoseUnit ?? StoredSettings.codingGlucoseUnit
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(date, forKey: .date)
+//        try container.encode(controllerTimeZone, forKey: .controllerTimeZone)
+//        try container.encode(dosingEnabled, forKey: .dosingEnabled)
+//        try container.encodeIfPresent(glucoseTargetRangeSchedule, forKey: .glucoseTargetRangeSchedule)
+//        try container.encodeIfPresent(preMealTargetRange?.doubleRange(for: bloodGlucoseUnit), forKey: .preMealTargetRange)
+//        try container.encodeIfPresent(workoutTargetRange?.doubleRange(for: bloodGlucoseUnit), forKey: .workoutTargetRange)
+//        try container.encodeIfPresent(overridePresets, forKey: .overridePresets)
+//        try container.encodeIfPresent(scheduleOverride, forKey: .scheduleOverride)
+//        try container.encodeIfPresent(preMealOverride, forKey: .preMealOverride)
+//        try container.encodeIfPresent(maximumBasalRatePerHour, forKey: .maximumBasalRatePerHour)
+//        try container.encodeIfPresent(maximumBolus, forKey: .maximumBolus)
+//        try container.encodeIfPresent(suspendThreshold, forKey: .suspendThreshold)
+//        try container.encodeIfPresent(insulinType, forKey: .insulinType)
+//        try container.encodeIfPresent(deviceToken, forKey: .deviceToken)
+//        try container.encodeIfPresent(defaultRapidActingModel, forKey: .defaultRapidActingModel)
+//        try container.encodeIfPresent(basalRateSchedule, forKey: .basalRateSchedule)
+//        try container.encodeIfPresent(insulinSensitivitySchedule, forKey: .insulinSensitivitySchedule)
+//        try container.encodeIfPresent(carbRatioSchedule, forKey: .carbRatioSchedule)
+//        try container.encodeIfPresent(notificationSettings, forKey: .notificationSettings)
+//        try container.encodeIfPresent(controllerDevice, forKey: .controllerDevice)
+//        try container.encodeIfPresent(cgmDevice.map { CodableDevice($0) }, forKey: .cgmDevice)
+//        try container.encodeIfPresent(pumpDevice.map { CodableDevice($0) }, forKey: .pumpDevice)
+//        try container.encode(bloodGlucoseUnit.unitString, forKey: .bloodGlucoseUnit)
+//        try container.encode(automaticDosingStrategy, forKey: .automaticDosingStrategy)
+//        try container.encode(syncIdentifier, forKey: .syncIdentifier)
+//    }
+
+    private enum CodingKeys: String, CodingKey {
+
+        case authorizationStatus
+        case soundSetting
+        case badgeSetting
+        case alertSetting
+        case notificationCenterSetting
+        case lockScreenSetting
+        case carPlaySetting
+        case alertStyle
+        case showPreviewsSetting
+        case criticalAlertSetting
+        case providesAppNotificationSettings
+        case announcementSetting
+        case timeSensitiveSetting
+        case scheduledDeliverySetting
+    }
+}
