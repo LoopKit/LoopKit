c73f3e6f3c48a94f3f2628c8f705c1a311d8f551
diggabyte <scott.paradis@trackvia.com>
2023-03-06 13:33:04 -0700

    Format emoji sources, added comments

--- LoopKitUI/CarbKit/FoodEmojiDataSource.swift
+++ LoopKitUI/CarbKit/FoodEmojiDataSource.swift
@@ -13,11 +13,32 @@ public func CarbAbsorptionInputController() -> EmojiInputController {
 private class FoodEmojiDataSource: EmojiDataSource {
     private static let fast: [String] = {
         var fast = [
-            "🍭", "🍬", "🍯",
-            "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍",
-            "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🥝",
-            "🌽", "🍿", "🍘", "🍡", "🍦", "🍧", "🎂", "🥠",
-            "☕️",
+            "🍭", // lollipop
+            "🍬", // candy
+            "🍯", // honey pot
+            "🍇", // grapes
+            "🍈", // melon
+            "🍉", // watermelon
+            "🍊", // tangerine
+            "🍋", // lemon
+            "🍌", // banana
+            "🍍", // pineapple
+            "🍎", // red apple
+            "🍏", // green apple
+            "🍐", // pear
+            "🍑", // peach
+            "🍒", // cherries
+            "🍓", // strawberry
+            "🥝", // kiwi fruit
+            "🌽", // ear of corn
+            "🍿", // popcorn
+            "🍘", // rice cracker
+            "🍡", // dango
+            "🍦", // soft ice cream
+            "🍧", // shaved ice
+            "🎂", // birthday cake
+            "🥠", // fortune cookie
+            "☕️", // coffee
         ]
 
         return fast
@@ -25,13 +46,48 @@ private class FoodEmojiDataSource: EmojiDataSource {
 
     private static let medium: [String] = {
         var medium = [
-            "🌮", "🍟", "🍳", "🍲", "🍱", "🍛",
-            "🍜", "🍠", "🍤", "🍥",
-            "🥪", "🥫", "🥟", "🥡", "🍢", "🍣",
-            "🍅", "🥔", "🥕", "🌶", "🥒", "🥗", "🍄", "🥦",
-            "🍆", "🥥", "🍞", "🥐", "🥖", "🥨", "🥞", "🍙", "🍚",
-            "🍼", "🥛", "🍮", "🥧",
-            "🍨", "🍩", "🍪", "🍰", "🍫",
+            "🌮", // taco
+            "🍟", // french fries
+            "🍳", // cooking
+            "🍲", // pot of food
+            "🍱", // bento box
+            "🍛", // curry rice
+            "🍜", // steaming bowl (ramen / noodle soup)
+            "🍠", // roasted sweet potato
+            "🍤", // fried shrimp
+            "🍥", // fish cake with swirl
+            "🥪", // sandwich
+            "🥫", // canned food
+            "🥟", // dumpling
+            "🥡", // takeout box
+            "🍢", // oden
+            "🍣", // sushi
+            "🍅", // tomato
+            "🥔", // potato
+            "🥕", // carrot
+            "🌶", // hot pepper
+            "🥒", // cucumber
+            "🥗", // green salad
+            "🍄", // mushroom
+            "🥦", // broccoli
+            "🍆", // eggplant
+            "🥥", // coconut
+            "🍞", // bread
+            "🥐", // croissant
+            "🥖", // baguette bread
+            "🥨", // pretzel
+            "🥞", // pancakes
+            "🍙", // rice ball
+            "🍚", // cooked rice
+            "🍼", // baby bottle
+            "🥛", // glass of milk
+            "🍮", // custard
+            "🥧", // pie
+            "🍨", // ice cream
+            "🍩", // doughnut
+            "🍪", // cookie
+            "🍰", // shortcake
+            "🍫", // chocolate bar
        ]
 
         return medium
@@ -39,8 +95,19 @@ private class FoodEmojiDataSource: EmojiDataSource {
 
     private static let slow: [String] = {
         var slow = [
-            "🍕", "🥑", "🥜", "🌰", "🧀", "🍖", "🍗", "🥓",
-            "🍔", "🌭", "🌯", "🍝", "🥩"
+            "🍕", // pizza
+            "🥑", // avocado
+            "🥜", // peanuts
+            "🌰", // chestnut
+            "🧀", // cheese wedge
+            "🍖", // meat on bone
+            "🍗", // poultry leg
+            "🥓", // bacon
+            "🍔", // hamburger
+            "🌭", // hot dog
+            "🌯", // burrito
+            "🍝", // spaghetti
+            "🥩" // cut of meat
         ]
 
         return slow
@@ -48,8 +115,19 @@ private class FoodEmojiDataSource: EmojiDataSource {
 
     private static let other: [String] = {
         var other = [
-            "🍶", "🍾", "🍷", "🍸", "🍺", "🍻", "🥂", "🥃",
-            "🍹", "🥣", "🥤", "🥢", "🍵",
+            "🍶", // sake
+            "🍾", // bottle with popping cork
+            "🍷", // wine glass
+            "🍸", // cocktail glass
+            "🍺", // beer mug
+            "🍻", // clinking beer mugs
+            "🥂", // clinking glasses
+            "🥃", // tumbler glass
+            "🍹", // tropical drink
+            "🥣", // bowl with spoon
+            "🥤", // cup with straw
+            "🥢", // chopsticks
+            "🍵", // teacup without handle
             "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣",
             "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"
         ]
