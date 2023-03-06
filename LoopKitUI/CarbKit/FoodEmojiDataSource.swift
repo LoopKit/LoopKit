61e2a05d68633cd752c797a5d54feb8dfd68c17e
Scott Paradis <857573+diggabyte@users.noreply.github.com>
2023-03-06 13:00:04 -0700

    Updated food emojis
    Added some missing foods, cleaned up file structure, added comments

--- LoopKitUI/CarbKit/FoodEmojiDataSource.swift
+++ LoopKitUI/CarbKit/FoodEmojiDataSource.swift
@@ -2,7 +2,7 @@
 //  FoodEmojiDataSource.swift
 //  LoopKit
 //
-//  Copyright © 2017 LoopKit Authors. All rights reserved.
+//  Copyright © 2023 LoopKit Authors. All rights reserved.
 //
 
 public func CarbAbsorptionInputController() -> EmojiInputController {
@@ -13,11 +13,41 @@ public func CarbAbsorptionInputController() -> EmojiInputController {
 private class FoodEmojiDataSource: EmojiDataSource {
     private static let fast: [String] = {
         var fast = [
-            "🍭", "🍬", "🍯",
-            "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍",
-            "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🥝",
-            "🌽", "🍿", "🍘", "🍡", "🍦", "🍧", "🎂", "🥠",
-            "☕️",
+            "🍭", // lollipop
+            "🧃", // juice box
+            "🍫", // chocolate
+            "🥤", // cup with straw (soda)
+            "🍬", // candy
+            "🍯", // honey
+            "🍦", // soft ice cream
+            "🍧", // shaved ice
+            "🍨", // ice cream
+            "🎂", // birthday cake
+            "🍰", // shotcake
+            "🍩", // doughnut
+            "🍪", // cookie
+            "🍇", // grapes
+            "🍈", // melon
+            "🍉", // watermelon
+            "🍊", // tangerine
+            "🍋", // lemon
+            "🍌", // bananna
+            "🫐", // blueberries
+            "🍍", // pineapple
+            "🍎", // red apple
+            "🍏", // green apple
+            "🍐", // pear
+            "🍑", // peach
+            "🍒", // cherries
+            "🍓", // strawberries
+            "🥝", // kiwi fruit
+            "🌽", // corn
+            "🍿", // popcorn
+            "🍘", // rice cake
+            "🥠", // fortune cookie
+            "🍡", // dango
+            "☕️", // coffee
+            "🫖", // tea
         ]
 
         return fast
@@ -25,13 +55,44 @@ private class FoodEmojiDataSource: EmojiDataSource {
 
     private static let medium: [String] = {
         var medium = [
-            "🌮", "🍟", "🍳", "🍲", "🍱", "🍛",
-            "🍜", "🍠", "🍤", "🍥",
-            "🥪", "🥫", "🥟", "🥡", "🍢", "🍣",
-            "🍅", "🥔", "🥕", "🌶", "🥒", "🥗", "🍄", "🥦",
-            "🍆", "🥥", "🍞", "🥐", "🥖", "🥨", "🥞", "🍙", "🍚",
-            "🍼", "🥛", "🍮", "🥧",
-            "🍨", "🍩", "🍪", "🍰", "🍫",
+            "🌮", // taco
+            "🍟", // french fries
+            "🍳", // cooking (eggs)
+            "🍲", // pot of food
+            "🍱", // bento box
+            "🍛", // curry and rice
+            "🍜", // steaming bowl
+            "🍠", // roasted sweet potato
+            "🍤", // fried shrimp
+            "🍥", // fish cake
+            "🍙", // rice ball
+            "🍚", // cooked rice
+            "🥪", // sandwich
+            "🥫", // canned food
+            "🥟", // dumpling
+            "🥡", // takeout box
+            "🍢", // oden
+            "🍣", // sushi
+            "🍅", // tomato
+            "🥔", // potato
+            "🥕", // carrot
+            "🌶", // hot pepper
+            "🥒", // cucumber
+            "🥗", // green salad
+            "🍄", // mushroom
+            "🥦", // broccoli
+            "🥬", // leafy green
+            "🍆", // eggplant
+            "🥥", // coconut
+            "🍞", // bread
+            "🥐", // croissant
+            "🥖", // baguette bread
+            "🥨", // pretzel
+            "🥞", // pancakes
+            "🍼", // baby bottle
+            "🥛", // glass of milk
+            "🍮", // custard
+            "🥧", // pie
        ]
 
         return medium
@@ -39,8 +100,20 @@ private class FoodEmojiDataSource: EmojiDataSource {
 
     private static let slow: [String] = {
         var slow = [
-            "🍕", "🥑", "🥜", "🌰", "🧀", "🍖", "🍗", "🥓",
-            "🍔", "🌭", "🌯", "🍝", "🥩"
+            "🍕", // pizza
+            "🥑", // avocado
+            "🥜", // peanuts
+            "🌰", // chestnut
+            "🧀", // cheese wedge
+            "🍖", // meat on bone
+            "🍗", // poultry leg
+            "🥓", // bacon
+            "🍔", // hamburger
+            "🌭", // hot dog
+            "🌯", // burrito
+            "🍝", // spaghetti
+            "🥩", // cut of meat
+            "🍳", // cooking (eggs)
         ]
 
         return slow
@@ -48,8 +121,23 @@ private class FoodEmojiDataSource: EmojiDataSource {
 
     private static let other: [String] = {
         var other = [
-            "🍶", "🍾", "🍷", "🍸", "🍺", "🍻", "🥂", "🥃",
-            "🍹", "🥣", "🥤", "🥢", "🍵",
+            "🍶", // sake
+            "🍾", // bottle with popping cork (champagne)
+            "🍷", // wine
+            "🍸", // cocktail
+            "🍺", // beer
+            "🍻", // clinking beer mugs
+            "🥂", // clinking glasses
+            "🥃", // tumbler glass
+            "🍹", // tropical drink
+            "🥣", // bowl with spoon
+            "🥤", // cup with straw
+            "🥢", // chopsticks
+            "🍵", // teacup without handle
+            "🍴", // fork and knife
+            "🍽", // fork and knife with plate
+            "🥄", // spoon
+            "😋"  // face savoring food
             "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣",
             "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"
         ]
