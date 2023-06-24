a0c797da82c88b730657e326ae6ae2d086eb62b2
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2023-06-24 19:44:56 -0400

    Update enact override method name

--- LoopKit/Service/Remote/RemoteActionDelegate.swift
+++ LoopKit/Service/Remote/RemoteActionDelegate.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public protocol RemoteActionDelegate: AnyObject {
-    func updateRemoteOverride(name: String, durationTime: TimeInterval?, remoteAddress: String) async throws
+    func enactRemoteOverride(name: String, durationTime: TimeInterval?, remoteAddress: String) async throws
     func cancelRemoteOverride() async throws
     func deliverRemoteCarbs(amountInGrams: Double, absorptionTime: TimeInterval?, foodType: String?, startDate: Date?) async throws
     func deliverRemoteBolus(amountInUnits: Double) async throws
