850ed0f530bf69d7bc565a62d69d2a9e70e297df
Cameron Ingham <camji555@gmail.com>
2023-06-27 15:55:56 -0700

    [LOOP-4667] Future Glucose Scenario Support to MockCGMManager

--- LoopTestingKit/TestingScenarioInstance.swift
+++ LoopTestingKit/TestingScenarioInstance.swift
@@ -6,33 +6,15 @@
 //  Copyright © 2019 LoopKit Authors. All rights reserved.
 //
 
-import Foundation
 import LoopKit
 
 public struct TestingScenarioInstance {
-    
     public var pastGlucoseSamples: [NewGlucoseSample]
     public var futureGlucoseSamples: [NewGlucoseSample]
     public var pumpEvents: [NewPumpEvent]
     public var carbEntries: [NewCarbEntry]
     public var deviceActions: [DeviceAction]
     public let shouldReloadManager: ReloadManager?
-    
-    public init(
-        pastGlucoseSamples: [NewGlucoseSample],
-        futureGlucoseSamples: [NewGlucoseSample],
-        pumpEvents: [NewPumpEvent],
-        carbEntries: [NewCarbEntry],
-        deviceActions: [DeviceAction],
-        shouldReloadManager: ReloadManager? = nil
-    ) {
-        self.pastGlucoseSamples = pastGlucoseSamples
-        self.futureGlucoseSamples = futureGlucoseSamples
-        self.pumpEvents = pumpEvents
-        self.carbEntries = carbEntries
-        self.deviceActions = deviceActions
-        self.shouldReloadManager = shouldReloadManager
-    }
     
     public var hasCGMData: Bool {
         !(pastGlucoseSamples + futureGlucoseSamples).isEmpty
