b851c83165a4194d4742c9e8162e78a9671544e0
marionbarker <marionbarker@earthlink.net>
2024-02-07 13:27:58 -0800

    add simulator settings to MockPump

--- MockKitUI/MockPumpManager+UI.swift
+++ MockKitUI/MockPumpManager+UI.swift
@@ -31,7 +31,7 @@ extension MockPumpManager: PumpManagerUI {
     }
 
     public func settingsViewController(bluetoothProvider: BluetoothProvider, colorPalette: LoopUIColorPalette, allowDebugFeatures: Bool, allowedInsulinTypes: [InsulinType]) -> PumpManagerViewController {
-        let settings = MockPumpManagerSettingsView(pumpManager: self, supportedInsulinTypes: allowedInsulinTypes, appName: appName)
+        let settings = MockPumpManagerSettingsView(pumpManager: self, supportedInsulinTypes: allowedInsulinTypes, appName: appName, allowDebugFeatures: allowDebugFeatures)
         let hostingController = DismissibleHostingController(content: settings, isModalInPresentation: false, colorPalette: colorPalette)
         hostingController.navigationItem.backButtonDisplayMode = .generic
         let nav = PumpManagerSettingsNavigationViewController(rootViewController: hostingController)
