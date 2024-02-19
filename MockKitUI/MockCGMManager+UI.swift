2f535b3ca46825e82e0dd1b5ef9daccd53a3f0ca
Marion Barker <marionbarker@earthlink.net>
2024-02-19 10:24:21 -0800

    Add Simulator Settings Row to DIY version for pump and CGM simulators (#525)
    * reduce long press duration to default, add instructions to mock pump/cgm
    
    * Revert "reduce long press duration to default, add instructions to mock pump/cgm"
    
    This reverts commit 1ceb6fb73089619039a3cefcd828474fde394bc4.
    
    * Device Details
    
    * Simulator Settings
    
    * add simulator settings to MockPump
    
    ---------
    
    Co-authored-by: Jonas Björkert <jonas@bjorkert.se>

--- MockKitUI/MockCGMManager+UI.swift
+++ MockKitUI/MockCGMManager+UI.swift
@@ -29,7 +29,7 @@ extension MockCGMManager: CGMManagerUI {
     }
 
     public func settingsViewController(bluetoothProvider: BluetoothProvider, displayGlucosePreference: DisplayGlucosePreference, colorPalette: LoopUIColorPalette, allowDebugFeatures: Bool) -> CGMManagerViewController {
-        let settings = MockCGMManagerSettingsView(cgmManager: self, displayGlucosePreference: displayGlucosePreference, appName: appName)
+        let settings = MockCGMManagerSettingsView(cgmManager: self, displayGlucosePreference: displayGlucosePreference, appName: appName, allowDebugFeatures: allowDebugFeatures)
         let hostingController = DismissibleHostingController(content: settings, isModalInPresentation: false, colorPalette: colorPalette)
         hostingController.navigationItem.backButtonDisplayMode = .generic
         let nav = CGMManagerSettingsNavigationViewController(rootViewController: hostingController)
