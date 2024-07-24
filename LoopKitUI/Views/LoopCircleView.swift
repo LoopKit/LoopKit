866951c1ffc780f8c71b4d45ca84f5cc5a534e1d
Cameron Ingham <camji555@gmail.com>
2024-07-24 15:19:37 -0700

    [LOOP-4884] Add Animation Support for LoopCircleView

--- LoopKitUI/Views/LoopCircleView.swift
+++ LoopKitUI/Views/LoopCircleView.swift
@@ -13,12 +13,22 @@ public struct LoopCircleView: View {
     @Environment(\.loopStatusColorPalette) private var loopStatusColors
     @Environment(\.isEnabled) private var isEnabled
     
-    let closedLoop: Bool
-    let freshness: LoopCompletionFreshness
+    @Binding private var animating: Bool
+    @Binding private var closedLoop: Bool
+    @Binding private var freshness: LoopCompletionFreshness
     
-    public init(closedLoop: Bool, freshness: LoopCompletionFreshness) {
-        self.closedLoop = closedLoop
-        self.freshness = freshness
+    public init(closedLoop: Binding<Bool>, freshness: Binding<LoopCompletionFreshness>, animating: Binding<Bool> = .constant(false)) {
+        self._closedLoop = closedLoop
+        self._freshness = freshness
+        self._animating = animating
+    }
+    
+    public init(closedLoop: Bool, freshness: LoopCompletionFreshness, animating: Bool = false) {
+        self.init(
+            closedLoop: .constant(closedLoop),
+            freshness: .constant(freshness),
+            animating: .constant(animating)
+        )
     }
     
     public var body: some View {
@@ -26,9 +36,12 @@ public struct LoopCircleView: View {
         
         Circle()
             .trim(from: closedLoop ? 0 : 0.2, to: 1)
-            .stroke(!isEnabled ? Color(UIColor.systemGray3) : loopColor, lineWidth: 8)
-            .rotationEffect(Angle(degrees: -126))
+            .stroke(!isEnabled ? Color(UIColor.systemGray3) : loopColor, lineWidth: animating && closedLoop ? 12 : 8)
+            .scaleEffect(animating && closedLoop ? 0.7 : 1)
+            .animation(.easeInOut(duration: 1).repeat(while: animating && closedLoop, autoreverses: true), value: animating)
             .frame(width: 36, height: 36)
+            .rotationEffect(Angle(degrees: closedLoop ? -90 : -126))
+            .animation(.default, value: closedLoop)
     }
     
     func getLoopColor(freshness: LoopCompletionFreshness) -> Color {
@@ -39,6 +52,16 @@ public struct LoopCircleView: View {
             return Color(uiColor: loopStatusColors.warning)
         case .stale:
             return Color(uiColor: loopStatusColors.error)
+        }
+    }
+}
+
+extension Animation {
+    func `repeat`(while expression: Bool, autoreverses: Bool = true) -> Animation {
+        if expression {
+            return self.repeatForever(autoreverses: autoreverses)
+        } else {
+            return self
         }
     }
 }
