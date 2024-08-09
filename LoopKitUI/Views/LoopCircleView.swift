bbca487616ef53408052955576106c0889010dd7
Cameron Ingham <camji555@gmail.com>
2024-08-09 13:19:53 -0700

    [LOOP-4987] Fix color cycle of Loop Status

--- LoopKitUI/Views/LoopCircleView.swift
+++ LoopKitUI/Views/LoopCircleView.swift
@@ -24,15 +24,24 @@ public struct LoopCircleView: View {
     }
     
     public var body: some View {
-        Circle()
-            .trim(from: closedLoop ? 0 : 0.2, to: 1)
-            .stroke(!isEnabled ? Color(UIColor.systemGray3) : loopColor, lineWidth: animating && closedLoop ? 12 : 8)
-            .scaleEffect(animating && closedLoop ? 0.7 : 1)
-            .rotationEffect(Angle(degrees: closedLoop ? -90 : -126))
-            .frame(width: 36, height: 36)
-            .animation(animating && closedLoop ? .easeInOut(duration: 1).repeatForever(autoreverses: true) : .easeInOut(duration: 1), value: animating)
-            .animation(.default, value: closedLoop)
-            .animation(.none, value: loopColor)
+        ZStack {
+            Circle()
+                .trim(from: closedLoop ? 0 : 0.2, to: 1)
+                .stroke(!isEnabled ? Color(UIColor.systemGray3) : loopColor, lineWidth: animating && closedLoop ? 12 : 8)
+                .scaleEffect(animating && closedLoop ? 0.7 : 1)
+                .rotationEffect(Angle(degrees: closedLoop ? -90 : -126))
+                .frame(width: 36, height: 36)
+                .animation(animating && closedLoop ? .easeInOut(duration: 1).repeatForever(autoreverses: true) : .easeInOut(duration: 1), value: animating)
+                .animation(.default, value: closedLoop)
+                .animation(.none, value: loopColor)
+            
+            VStack {
+                Text(animating.description)
+                Text(closedLoop.description)
+                Text(freshness.description)
+            }
+            .font(.footnote)
+        }
     }
     
     private var loopColor: Color {
@@ -43,6 +52,19 @@ public struct LoopCircleView: View {
             return Color(uiColor: loopStatusColors.warning)
         case .stale:
             return Color(uiColor: loopStatusColors.error)
+        }
+    }
+}
+
+extension LoopCompletionFreshness {
+    var description: String {
+        switch self {
+        case .fresh:
+            return "fresh"
+        case .aging:
+            return "aging"
+        case .stale:
+            return "stale"
         }
     }
 }
