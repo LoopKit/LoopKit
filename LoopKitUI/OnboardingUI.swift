da42f7306cc40c621bc73972637812f75c95334f
Pete Schwamb <pete@schwamb.net>
2024-10-25 14:38:39 -0500

    LOOP-5122 Onboarding provider is plugin host (#621)
    * OnboardingProvider should conform to PluginHost
    
    * Remove unused method

--- LoopKitUI/OnboardingUI.swift
+++ LoopKitUI/OnboardingUI.swift
@@ -119,13 +119,6 @@ public protocol ServiceProvider: AnyObject {
 
     /// The descriptor list of available services.
     var availableServices: [ServiceDescriptor] { get }
-
-    /// Onboard the service with the specified identifier.
-    ///
-    /// - Parameters:
-    ///     - identifier: The identifier of the service to onboard.
-    /// - Returns: Either a conforming view controller to onboard the service, a newly onboarded service, or an error.
-    func onboardService(withIdentifier identifier: String) -> Result<OnboardingResult<ServiceViewController, Service>, Error>
 }
 
 public protocol TherapySettingsProvider {
@@ -136,7 +129,7 @@ public protocol SupportProvider: AnyObject {
     var availableSupports: [SupportUI] { get }
 }
 
-public protocol OnboardingProvider: NotificationAuthorizationProvider, HealthStoreAuthorizationProvider, BluetoothProvider, CGMManagerProvider, PumpManagerProvider, StatefulPluggableProvider, ServiceProvider, TherapySettingsProvider, SupportProvider {
+public protocol OnboardingProvider: NotificationAuthorizationProvider, HealthStoreAuthorizationProvider, BluetoothProvider, CGMManagerProvider, PumpManagerProvider, StatefulPluggableProvider, ServiceProvider, TherapySettingsProvider, SupportProvider, PluginHost {
     var allowDebugFeatures: Bool { get }   // NOTE: DEBUG FEATURES - DEBUG AND TEST ONLY
 }
 
