aaf6c7d6a855e64d8ba5b2441444f91bf3b86ab9
Cameron Ingham <camji555@gmail.com>
2025-07-29 13:24:58 -0700

    [LOOP-5358] Fix missing `id` field on migration

---
+++ LoopKit/Persistence/ModelMappingv5Tov6Policy.swift
@@ -0,0 +1,44 @@
+//
+//  ModelMappingv5Tov6Policy.swift
+//  LoopKit
+//
+//  Created by Cameron Ingham on 7/29/25.
+//  Copyright © 2025 LoopKit Authors. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DosingDecisionObjectMigrationPolicy: NSEntityMigrationPolicy {
+    override func createDestinationInstances(
+        forSource sInstance: NSManagedObject,
+        in mapping: NSEntityMapping,
+        manager: NSMigrationManager
+    ) throws {
+        try super.createDestinationInstances(
+            forSource: sInstance,
+            in: mapping,
+            manager: manager
+        )
+        
+        guard let destInstance = manager.destinationInstances(
+            forEntityMappingName: mapping.name,
+            sourceInstances: [sInstance]
+        ).first else {
+            return
+        }
+        
+        // Assuming your JSON data is stored in `data` attribute as Data
+        if let jsonData = sInstance.value(forKey: "data") as? Data {
+            let decoder = JSONDecoder()
+            struct Payload: Decodable { let id: UUID }
+            
+            if let payload = try? decoder.decode(
+                Payload.self,
+                from: jsonData
+            ) {
+                destInstance.setValue(payload.id, forKey: "id")
+            }
+        }
+    }
+}
