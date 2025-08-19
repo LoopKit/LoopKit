d1c4d064c9c920632ffc2c4fb1d39bb8b9fa83aa
tnichol00 <tnichol00@gmail.com>
2025-08-19 11:26:26 -0400

    Updated ExponentialInsulinModelPreset.swift
    Set actionDuration of rapidActingAdult to 300

--- LoopKit/Insulin/ExponentialInsulinModelPreset.swift
+++ LoopKit/Insulin/ExponentialInsulinModelPreset.swift
@@ -21,7 +21,7 @@ extension ExponentialInsulinModelPreset {
     public var actionDuration: TimeInterval {
         switch self {
         case .rapidActingAdult:
-            return .minutes(240)
+            return .minutes(300)
         case .rapidActingChild:
             return .minutes(360)
         case .fiasp:
